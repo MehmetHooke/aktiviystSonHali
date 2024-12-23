6f71a269faf2bf93ea717107e4d089d7cf274746
pianosensev1 <hoke628@gmail.com>
2024-12-23 20:01:43 +0300

    first commit

--- app/src/main/java/com/example/aktivist/EventsApiService.kt
+++ app/src/main/java/com/example/aktivist/EventsApiService.kt
@@ -1,4 +1,4 @@
 package com.example.aktivist
 
-class RetrofitInstance {
+class EventsApiService {
 }
--- app/src/main/java/com/example/aktivist/TicketmasterResponse.kt
+++ app/src/main/java/com/example/aktivist/TicketmasterResponse.kt
@@ -1,36 +1,4 @@
-data class TicketmasterResponse(
-    val _embedded: Embedded?
-)
+package com.example.aktivist
 
-data class Embedded(
-    val events: List<Event>?
-)
-
-data class Event(
-    val name: String,
-    val dates: EventDates,
-    val info: String?,
-    val classifications: List<Classification>?,
-    val images: List<Image>?
-)
-
-data class EventDates(
-    val start: Start
-)
-
-data class Start(
-    val localDate: String,
-    val localTime: String?
-)
-
-data class Classification(
-    val segment: Segment
-)
-
-data class Segment(
-    val name: String
-)
-
-data class Image(
-    val url: String
-)
+class TicketmasterResponse {
+}
